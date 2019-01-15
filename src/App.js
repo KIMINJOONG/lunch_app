4e075a87da9fd78cddbdef502a1f4e409e7519fe
KIMINJOONG <ijboym216@gmail.com>
2019-01-15 23:16:37 +0900

    점심뭐먹지 프로젝트 레이아웃

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <div className="container">
+          <div className="banner">
+          </div>
+          <div className="main">
+            <div className="leftMenu">
+              <span>왼쪽메뉴</span>
+            </div>
+            <div className="map">
+              <span>지도영역</span>
+            </div>
+            <div className="list">
+              <span>리스트 영역</span>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
